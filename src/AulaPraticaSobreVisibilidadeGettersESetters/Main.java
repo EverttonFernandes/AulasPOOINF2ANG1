d437e257ed562e4b740978cdb3f1142a405a9026
EverttonFernandes <evertonoliveirafernandes@outlook.com>
2025-04-04 08:30:56 -0300

    Exemplo simples de visibilidade com uso de métodos getters e setters

---
+++ src/AulaPraticaSobreVisibilidadeGettersESetters/Main.java
@@ -0,0 +1,14 @@
+package AulaPraticaSobreVisibilidadeGettersESetters;
+
+public class Main {
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa("875.333.123-50", "876345102", "Lucas", "Rua C");
+        System.out.println(pessoa.getCpf());
+        System.out.println(pessoa.getRg());
+        System.out.println(pessoa.getEndereco());
+
+        // Como mudar o endereço do Lucas? Usamos o método setter para isso!
+        pessoa.setEndereco("Rua General Balbao 111");
+        System.out.println(pessoa.getEndereco());
+    }
+}
---
+++ src/AulaPraticaSobreVisibilidadeGettersESetters/Pessoa.java
@@ -0,0 +1,35 @@
+package AulaPraticaSobreVisibilidadeGettersESetters;
+
+public class Pessoa {
+    private String cpf;
+    private String rg;
+    private String nome;
+    private String endereco;
+
+    public Pessoa(String cpf, String rg, String nome, String endereco) {
+        this.cpf = cpf;
+        this.rg = rg;
+        this.nome = nome;
+        this.endereco = endereco;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
