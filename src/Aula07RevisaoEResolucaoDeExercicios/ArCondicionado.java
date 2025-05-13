030169adebdbcf11ae2c95498bac165cec14e6c3
EverttonFernandes <evertonoliveirafernandes@outlook.com>
2025-05-13 08:32:20 -0300

    Resolucao do exercicio sobre laboratorio com alunos e computadores

---
+++ src/Aula07RevisaoEResolucaoDeExercicios/ArCondicionado.java
@@ -0,0 +1,13 @@
+package Aula07RevisaoEResolucaoDeExercicios;
+
+public class ArCondicionado {
+    private String marca;
+
+    public ArCondicionado(String marca) {
+        this.marca = marca;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+}
---
+++ src/Aula07RevisaoEResolucaoDeExercicios/Computador.java
@@ -0,0 +1,13 @@
+package Aula07RevisaoEResolucaoDeExercicios;
+
+public class Computador {
+    private String marca;
+
+    public Computador(String marca) {
+        this.marca = marca;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+}
