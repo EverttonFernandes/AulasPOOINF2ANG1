0114a46ada898e88e8c56b4be776071d95245842
EverttonFernandes <evertonoliveirafernandes@outlook.com>
2025-08-22 08:28:36 -0300

    Descricao método estático

--- src/Aula12MetodosEstaticosNaPratia/Main.java
+++ src/Aula12MetodosEstaticosNaPratia/Main.java
@@ -5,6 +5,15 @@ public class Main {
         CalculadoraNormal calculadoraNormal = new CalculadoraNormal();
         calculadoraNormal.somar(5, 10);
 
-        CalculadoraComMetodoEstatico.somar(5, 10);
+        CalculadoraComMetodoEstatico.somar(2, 2);
+        // Quando o método é estático, não precisamos instanciar o objeto.
+        // Quando o método é estático, precisamos criar uma instância da classe para chamar o método.
+
+        // Usamos métodos estáticos em situações onde gostariamos de ter um comportamento genérico
+        // em diferentes lugares, como por exemplo, somar, multiplicar ou fazer qualquer outra
+        // operacao matematica para uma loja, uma lancheria, um banco, uma lotérica, escola e etc.
+        // com isto não precisamos nos acoplar diretamente a classe, basta chamar o método estático.
+
+
     }
 }
